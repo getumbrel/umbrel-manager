2254c16881f8e64e7b9f6ce1eda93ccf07dbaa3a
Lounès Ksouri <dev@louneskmt.com>
2020-10-03 20:55:59 +0200

    Added utils/price endpoint
    Requests Bitcoin price through Tor

---
+++ routes/v1/utils.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('middlewares/auth.js');
+
+const constants = require('utils/const.js');
+const safeHandler = require('utils/safeHandler');
+
+const {SocksProxyAgent} = require('socks-proxy-agent');
+const axios = require('axios');
+
+const agent = new SocksProxyAgent(`socks5h://${constants.TOR_PROXY_IP}:${constants.TOR_PROXY_PORT}`);
+
+router.get('/price', auth.jwt, safeHandler(async(req, res) => {
+  const response = await axios({
+    url: 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD',
+    httpsAgent: agent,
+    method: 'GET'
+  });
+
+  if (response.data) {
+    return res.status(constants.STATUS_CODES.OK).json(response.data);
+  }
+
+  return res.status(constants.STATUS_CODES.BAD_GATEWAY).json();
+}));
+
+module.exports = router;
