538c475ed629a73f09e5670912983ed78742090b
Lounès Ksouri <dev@louneskmt.com>
2021-04-17 20:03:58 +0200

    Remove unused constant

--- utils/const.js
+++ utils/const.js
@@ -33,7 +33,6 @@ module.exports = {
   UPDATE_SIGNAL_FILE: process.env.UPDATE_SIGNAL_FILE || '/signals/update',
   UPDATE_LOCK_FILE: process.env.UPDATE_LOCK_FILE || '/statuses/update-in-progress',
   BACKUP_STATUS_FILE: process.env.BACKUP_STATUS_FILE || '/statuses/backup-status.json',
-  DEBUG_SIGNAL_FILE: process.env.DEBUG_SIGNAL_FILE || "/signals/debug",
   DEBUG_STATUS_FILE: process.env.DEBUG_STATUS_FILE || "/statuses/debug-status.json",
   DEBUG_RESULT_FILE: process.env.DEBUG_RESULT_FILE || "/statuses/debug-result",
   TOR_PROXY_IP: process.env.TOR_PROXY_IP || '192.168.0.1',
