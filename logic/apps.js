4cf0f5dda97679a92c9a92fd1b419cc4f7b6360f
Aaron Dewes <aaron.dewes@web.de>
2021-05-29 22:02:28 +0200

    WIP: generate registry.json from app.yml files
    This also makes readJsonFile read YAML

--- logic/apps.js
+++ logic/apps.js
@@ -2,6 +2,10 @@ const diskLogic = require('logic/disk.js');
 const NodeError = require('models/errors.js').NodeError;
 
 async function get(query) {
+  const registryExists = await diskLogic.checkAppRegistry();
+  if (!registryExists) {
+    await generateRegisty();
+  }
   let apps = await diskLogic.readAppRegistry();
 
   // Do all hidden service lookups concurrently
@@ -48,10 +52,42 @@ async function uninstall(id) {
   } catch (error) {
     throw new NodeError('Could not write the signal file');
   }
-};
+}
+
+async function generateRegisty() {
+  const apps = diskLogic.listAppsInAppsDir();
+  let appRegistry;
+  apps.forEach(app => {
+    const parsedInfo = {};
+    const appInfo = diskLogic.readAppInfo(app);
+    if (appInfo.version !== 0 || !appInfo.meta) {
+      console.warn(`${app} is using incompatible app.yml (Version ${appInfo.version})`);
+
+      return;
+    }
+    ['category', 'name', 'version', 'tagline', 'description', 'developer', 'repo', 'support', 'port'].forEach(property => {
+      if (!appInfo.meta[property]) {
+        console.warn(`${app} does not define required property meta.${property}`);
+
+        return;
+      }
+      parsedInfo[property] = appInfo.meta[property];
+    });
+    parsedInfo.id = app;
+
+    // Optional properties
+    parsedInfo.dependencies = appInfo.meta.dependencies || [];
+    parsedInfo.defaultPassword = appInfo.meta.defaultPassword || undefined;
+    parsedInfo.path = appInfo.meta.path || '/';
+    parsedInfo.gallery = appInfo.meta.galler || ['1.jpg', '2.jpg', '3.jpg'];
+    appRegistry.push(parsedInfo);
+  });
+  await diskLogic.writeAppRegistry(appRegistry);
+}
 
 module.exports = {
   get,
   install,
   uninstall,
+  generateRegisty
 };
