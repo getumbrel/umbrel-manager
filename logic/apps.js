03774d7bc6b8f50a1ae9d5da86be1c3a12419b08
Luke Childs <lukechilds123@gmail.com>
2021-11-15 18:18:50 +0700

    Derive deterministic app passwords

--- logic/apps.js
+++ logic/apps.js
@@ -1,5 +1,6 @@
 const diskLogic = require('logic/disk.js');
 const NodeError = require('models/errors.js').NodeError;
+const deriveEntropy = require('modules/derive-entropy');
 
 async function get(query) {
   let apps = await diskLogic.readAppRegistry();
@@ -10,6 +11,15 @@ async function get(query) {
       app.hiddenService = await diskLogic.readHiddenService(`app-${app.id}`);
     } catch(e) {
       app.hiddenService = '';
+    }
+  }));
+
+  // Derive all passwords concurrently
+  await Promise.all(apps.filter(app => app.deterministicPassword).map(async app => {
+    try {
+      app.defaultPassword = await deriveEntropy(`app-${app.id}-seed-APP_PASSWORD`);
+    } catch(e) {
+      app.defaultPassword = '';
     }
   }));
 
