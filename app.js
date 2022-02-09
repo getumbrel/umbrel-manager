100570798ba35f49efcbee1387db450b481b4d4b
Luke Childs <lukechilds123@gmail.com>
2022-02-09 19:14:09 +0700

    Disable CORS (#118)

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const cors = require('cors');
 
 // Keep requestCorrelationId middleware as the first middleware. Otherwise we risk losing logs.
 const requestCorrelationMiddleware = require('middlewares/requestCorrelationId.js'); // eslint-disable-line id-length
 const camelCaseReqMiddleware = require('middlewares/camelCaseRequest.js').camelCaseRequest;
-const corsOptions = require('middlewares/cors.js').corsOptions;
 const errorHandleMiddleware = require('middlewares/errorHandling.js');
 require('middlewares/auth.js');
 
@@ -25,9 +23,6 @@ const external = require('routes/v1/external.js');
 const apps = require('routes/v1/apps.js');
 
 const app = express();
-
-// Handles CORS
-app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
